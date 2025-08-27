ad9497fb634fcab64db9ca5e7123eddb5a92b2d8
Sleither Reyes <sleither.reyes@gmx.de>
2025-08-27 11:47:35 +0200

    feat(init): disable spring security until auth is implemented

---
+++ src/main/java/de/dicecup/classlink/NoSecurityDevConfig.java
@@ -0,0 +1,24 @@
+package de.dicecup.classlink;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@Profile("dev")
+public class NoSecurityDevConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
