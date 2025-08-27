9d272635f5e866decae0ba4115b091598e30f702
Sleither Reyes <sleither.reyes@gmx.de>
2025-08-27 11:40:25 +0200

    init: spring boot base setup

---
+++ src/main/java/de/dicecup/classlink/ClasslinkApplication.java
@@ -0,0 +1,13 @@
+package de.dicecup.classlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasslinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClasslinkApplication.class, args);
+    }
+
+}
