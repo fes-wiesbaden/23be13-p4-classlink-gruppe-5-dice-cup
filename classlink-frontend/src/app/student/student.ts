c732227d1881a1518fe7cb717e3224ed9a15e09e
Emil Ruckriegl <emil.ruckriegl@sinc.de>
2025-08-28 09:52:32 +0200

    feat(basic-layout): added basic routing for student/teacher/admin

---
+++ classlink-frontend/src/app/student/student.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+   standalone: true,
+  selector: 'app-student',
+  imports: [],
+  templateUrl: './student.html',
+  styleUrl: './student.scss'
+})
+export class StudentComponent {
+
+}
---
+++ classlink-frontend/src/app/teacher/teacher.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-teacher',
+  imports: [],
+  templateUrl: './teacher.html',
+  styleUrl: './teacher.scss'
+})
+export class TeacherComponent {
+
+}
