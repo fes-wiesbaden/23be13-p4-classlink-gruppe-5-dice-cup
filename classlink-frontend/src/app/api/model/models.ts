c95e9eed2f26fa6ca9816a5d5792adeba908fe78
Sleither Reyes <sleither.reyes@gmx.de>
2025-09-10 01:35:56 +0200

    feat(api): integrate OpenAPI-generated Angular client for user management and registration
    - Added auto-generated API services and models for user management and registration.
    - Included `.gitignore` and `.openapi-generator-ignore` for OpenAPI artifacts.
    - Implemented `ApiModule`, configuration, and custom HTTP parameter encoder.
    - Configured base `ApiModule` for DI and standalone use.
    - Added OpenAPI client scripts and setup files.

---
+++ classlink-frontend/src/app/api/model/models.ts
@@ -0,0 +1,6 @@
+export * from './create-user-info-dto';
+export * from './invite-request-dto';
+export * from './invite-response-dto';
+export * from './regristration-reques-dto';
+export * from './user-dto';
+export * from './user-info-dto';
