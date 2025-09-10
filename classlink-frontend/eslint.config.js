99001ce3eb6348d8ff7767d58f178eaa0a419fd2
Sleither Reyes <sleither.reyes@gmx.de>
2025-09-10 11:17:05 +0200

    chore(eslint): exclude generated API folder from linting
    - Updated ESLint configuration to ignore the `src/app/api` folder.
    - Improved readability and formatting of ESLint setup.

--- classlink-frontend/eslint.config.js
+++ classlink-frontend/eslint.config.js
@@ -4,40 +4,35 @@ const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
 module.exports = tseslint.config(
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+    {
+        files: ["**/*.ts"],
+        ignores: ["src/app/api/**"],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.stylistic,
+            ...angular.configs.tsRecommended,
+        ],
+        processor: angular.processInlineTemplates,
+        rules: {
+            "@angular-eslint/directive-selector": [
+                "error",
+                { type: "attribute", prefix: "app", style: "camelCase" },
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                { type: "element", prefix: "app", style: "kebab-case" },
+            ],
         },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
     },
-  },
-  {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
+
+    // Angular Templates (HTML)
+    {
+        files: ["**/*.html"],
+        extends: [
+            ...angular.configs.templateRecommended,
+            ...angular.configs.templateAccessibility,
+        ],
+        rules: {},
+    }
 );
