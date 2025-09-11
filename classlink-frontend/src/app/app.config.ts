ca4491fb501ce87bdd805520f1bde76dc48d2de4
Sleither Reyes <sleither.reyes@gmx.de>
2025-09-11 14:29:04 +0200

    chore: integrate Storybook and update Angular dependencies
    - Added Storybook configuration to `angular.json` with build and serve options.
    - Updated `app.config.ts` to include PrimeNG setup, HTTP client, and API provider.
    - Upgraded multiple Angular-related packages and dependencies in `yarn.lock` to latest versions.

--- classlink-frontend/src/app/app.config.ts
+++ classlink-frontend/src/app/app.config.ts
@@ -1,7 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import {provideRouter} from "@angular/router";
+import {routes} from "./app.routes";
+import {provideHttpClient} from "@angular/common/http";
+import {provideApi} from "./api";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+    providers: [
+        provideRouter(routes),
+        provideHttpClient(),
+        provideApi('http://localhost:4000'),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
+    ]
 };
