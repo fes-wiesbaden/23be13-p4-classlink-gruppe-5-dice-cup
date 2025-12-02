82dc399f87fe48b3673b7513242c3ca61e328b01
Sleither Reyes <sleither.reyes@gmx.de>
2025-12-03 00:05:26 +0100

    feat(config): add BASE_PATH provider for API integration
    - Added `BASE_PATH` configuration pointing to `http://localhost:4000` for backend API integration.
    
    Signed-off-by: Sleither Reyes <sleither.reyes@gmx.de>

--- classlink-frontend/src/app/app.config.ts
+++ classlink-frontend/src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/http/auth.interceptor';
+import {BASE_PATH} from './api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // Prepare for backend integration
     provideHttpClient(withInterceptors([authInterceptor])),
+      {provide: BASE_PATH, useValue: 'http://localhost:4000'},
   ],
 };
