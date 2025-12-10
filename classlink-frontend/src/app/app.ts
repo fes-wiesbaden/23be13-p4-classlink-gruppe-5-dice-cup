8b3f0a1bccc3dad88315726b942ac18138758a14
Lukas Müller <Lukas.Mueller@sinc.de>
2025-12-10 10:31:16 +0100

    Layout Änderungen angepasst: Positionierung Überschriften, Positionierung DiceCup Logo etc.

--- classlink-frontend/src/app/app.ts
+++ classlink-frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DevDockComponent } from './components/dev-dock/dev-dock';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DevDockComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
