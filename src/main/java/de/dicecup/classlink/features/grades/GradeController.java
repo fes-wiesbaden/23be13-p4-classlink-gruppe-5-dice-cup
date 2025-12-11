1841ffc3fba8d70526c5aaed63006d16f0cf07ee
Marcel Plenert <madplenert@gmail.com>
2025-12-11 08:03:07 +0100

    Update GradeController.java

--- src/main/java/de/dicecup/classlink/features/grades/GradeController.java
+++ src/main/java/de/dicecup/classlink/features/grades/GradeController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+//Author: Marcel Plenert
+// Creates API Endpoint for Grade and Assignment functionality
 @RestController
 @RequestMapping("/api/assignment")
 @RequiredArgsConstructor
